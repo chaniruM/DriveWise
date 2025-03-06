40dbe7a9da296023b3703e1dceea638f1d108817
chaniruM <chanirumannapperuma18@gmail.com>
2025-03-06 20:32:45 +0530

    removed commented code
    removed unnecessary commented code

--- lib/pages/my_cars.dart
+++ lib/pages/my_cars.dart
@@ -1,87 +1,8 @@
-// import 'package:drivewise/pages/user_details_page.dart';
-// import 'package:drivewise/pages/home_page.dart';
-// import 'package:flutter/material.dart';
-//
-// class MyCarsPage extends StatelessWidget {
-//   const MyCarsPage({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('My Cars'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             Expanded(
-//               child: GridView.builder(
-//                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//                   crossAxisCount: 2,
-//                   crossAxisSpacing: 16.0,
-//                   mainAxisSpacing: 16.0,
-//                 ),
-//                 itemCount: 4,
-//                 itemBuilder: (context, index) {
-//                   return GestureDetector(
-//                     onTap: () {
-//                       // Navigate to the vehicle information page
-//                       print('Vehicle $index tapped');
-//                     },
-//                     child: Card(
-//                       elevation: 4.0,
-//                       shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(10.0),
-//                       ),
-//                       child: Column(
-//                         crossAxisAlignment: CrossAxisAlignment.center,
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           Expanded(
-//                             child: Image.asset(
-//                               'assets/car_placeholder.png', // Replace with image
-//                               fit: BoxFit.cover,
-//                             ),
-//                           ),
-//                           const SizedBox(height: 8.0),
-//                           Text(
-//                             'Vehicle ${index + 1}',
-//                             style: const TextStyle(
-//                               fontSize: 16.0,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   );
-//                 },
-//               ),
-//             ),
-//             const SizedBox(height: 16.0),
-//             ElevatedButton(
-//               onPressed: () {
-//                 // Navigate to add new vehicle page or show add vehicle dialog
-//                 print('Add New Vehicle tapped');
-//               },
-//               child: const Text('Add New Vehicle'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:drivewise/pages/register_vehicle_page.dart';
 import 'package:drivewise/pages/vehicle_datails_page.dart';
 import 'package:flutter/material.dart';
 import '../models/vehicle.dart';
 import '../widgets/vehicle_card.dart';
-// import 'register_vehicle_screen.dart';
-// import 'vehicle_details_page.dart';
 
 class MyCarsPage extends StatelessWidget {
   // Dummy data for demonstration purposes
@@ -126,64 +47,6 @@ class MyCarsPage extends StatelessWidget {
     ),
   ];
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text('My Cars'),
-  //     ),
-  //     body: Padding(
-  //       padding: const EdgeInsets.all(16.0),
-  //       child: Column(
-  //         crossAxisAlignment: CrossAxisAlignment.start,
-  //         children: [
-  //           Text(
-  //             'Your Vehicles',
-  //             style: TextStyle(
-  //               fontSize: 24,
-  //               fontWeight: FontWeight.bold,
-  //             ),
-  //           ),
-  //           SizedBox(height: 16),
-  //           Expanded(
-  //             child: GridView.builder(
-  //               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-  //                 crossAxisCount: 2,
-  //                 crossAxisSpacing: 16,
-  //                 mainAxisSpacing: 16,
-  //                 childAspectRatio: 0.75,
-  //               ),
-  //               itemCount: vehicles.length,
-  //               itemBuilder: (context, index) {
-  //                 return VehicleCard(
-  //                   vehicle: vehicles[index],
-  //                   onTap: () {
-  //                     Navigator.push(
-  //                       context,
-  //                       MaterialPageRoute(
-  //                         builder: (context) => VehicleDetailsPage(vehicle: vehicles[index]),
-  //                       ),
-  //                     );
-  //                   },
-  //                 );
-  //               },
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //     floatingActionButton: FloatingActionButton(
-  //       onPressed: () {
-  //         Navigator.push(
-  //           context,
-  //           MaterialPageRoute(builder: (context) => RegisterVehiclePage()),
-  //         );
-  //       },
-  //       child: Icon(Icons.add),
-  //       tooltip: 'Register New Vehicle',
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
